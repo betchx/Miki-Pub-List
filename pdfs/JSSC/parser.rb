56dc49bde45cdb12bbaf3cf076b9c60cb215d14a
tanabe <tanabe.atsushi@gmail.com>
2011-12-09 14:29:20 +0900

    インデントの修正
    ARGVループによるもの．
    
    変更がわかりやすくなる様に別コミットとした

--- pdfs/JSSC/parser.rb
+++ pdfs/JSSC/parser.rb
@@ -27,70 +27,70 @@ KKK
 
 ARGV.each do |uri|
 
-#file = Tempfile.new("journal-archive-work")
-file = open("resent.html","w+")
-open(uri){|u|
-  file.write NKF.nkf("-w",u.read)
-}
-file.rewind
+  #file = Tempfile.new("journal-archive-work")
+  file = open("resent.html","w+")
+  open(uri){|u|
+    file.write NKF.nkf("-w",u.read)
+  }
+  file.rewind
 
-html = Hpricot(file)
+  html = Hpricot(file)
 
-file.close
+  file.close
 
-doc =Hpricot( (html/"body").inner_html )
+  doc =Hpricot( (html/"body").inner_html )
 
-ttls = (doc/'td.blackbd').select{|x| (x/:a).size > 0}.map{|x| x.inner_text}
-paper_list = (doc/'td.black').select{|x| x.inner_text =~ /pp\./}
-links = paper_list.map{|x| (x/'a').map{|a| a['href']} }
-bodies = paper_list.map{|x| x.inner_text}
+  ttls = (doc/'td.blackbd').select{|x| (x/:a).size > 0}.map{|x| x.inner_text}
+  paper_list = (doc/'td.black').select{|x| x.inner_text =~ /pp\./}
+  links = paper_list.map{|x| (x/'a').map{|a| a['href']} }
+  bodies = paper_list.map{|x| x.inner_text}
 
-data = bodies.map{|x| x.gsub(/\n/,'|')}.join("\n").scan(/^([^|]+)\|([^,]+),.*Vol\.\D*(\d+).*\((\d+)\).*No\.\D*(\d+).*pp\.\D*(\d+-\d+)/)
+  data = bodies.map{|x| x.gsub(/\n/,'|')}.join("\n").scan(/^([^|]+)\|([^,]+),.*Vol\.\D*(\d+).*\((\d+)\).*No\.\D*(\d+).*pp\.\D*(\d+-\d+)/)
 
-dir = File.basename(Dir.pwd)
+  dir = File.basename(Dir.pwd)
 
-ttls.size.times do |i|
-  title = ttls[i].gsub(/:/,'：')
-  #$stderr.puts links[i]
-  abstract_link, pdf_link = *links[i]
-  authors,journal,volume,year,number,pages = *data[i]
-  next unless pages
-  #authors = NKF.nkf("-Ws", authors)
-  first_page, last_page = * pages.split(/-+/)
-  if year == volume
-    volume = number
-    number = nil   # for JSCE
-  end
-  tag = [year,volume,first_page].join('_')
-  pdf_file = "#{tag}_#{title}.pdf"
-  unless File.file?(pdf_file)
-    if $down
-      $stderr.puts "Fetching #{pdf_file}"
-      $stderr.puts "from: #{pdf_link}"
-      open(pdf_link,"rb") do |efile|
-        open(pdf_file,"wb") do |out|
-          out.write(efile.read)
-          out.close
+  ttls.size.times do |i|
+    title = ttls[i].gsub(/:/,'：')
+    #$stderr.puts links[i]
+    abstract_link, pdf_link = *links[i]
+    authors,journal,volume,year,number,pages = *data[i]
+    next unless pages
+    #authors = NKF.nkf("-Ws", authors)
+    first_page, last_page = * pages.split(/-+/)
+    if year == volume
+      volume = number
+      number = nil   # for JSCE
+    end
+    tag = [year,volume,first_page].join('_')
+    pdf_file = "#{tag}_#{title}.pdf"
+    unless File.file?(pdf_file)
+      if $down
+        $stderr.puts "Fetching #{pdf_file}"
+        $stderr.puts "from: #{pdf_link}"
+        open(pdf_link,"rb") do |efile|
+          open(pdf_file,"wb") do |out|
+            out.write(efile.read)
+            out.close
+          end
         end
       end
     end
+    #pdf_file = NKF.nkf("-Ws",pdf_file)
+    #title = {#{NKF.nkf("-Ws",title)}},
+    #journal = {#{NKF.nkf("-Ws",journal)}},
+    puts "@ARTICLE{#{tag},\r"
+    field "author", authors.split(/,/).join(' and ')
+    field "title", title
+    field "journal", journal
+    field "year", year
+    field "volume", volume
+    field "pages", pages
+    field "number", number if number
+    field "url", abstract_link
+    field "memo", "PDF_LINK: #{pdf_link}"
+    field "file", "#{pdf_file}:#{dir}\\\\#{pdf_file}:PDF"
+    puts "}\r\n\r"
   end
-  #pdf_file = NKF.nkf("-Ws",pdf_file)
-  #title = {#{NKF.nkf("-Ws",title)}},
-  #journal = {#{NKF.nkf("-Ws",journal)}},
-  puts "@ARTICLE{#{tag},\r"
-  field "author", authors.split(/,/).join(' and ')
-  field "title", title
-  field "journal", journal
-  field "year", year
-  field "volume", volume
-  field "pages", pages
-  field "number", number if number
-  field "url", abstract_link
-  field "memo", "PDF_LINK: #{pdf_link}"
-  field "file", "#{pdf_file}:#{dir}\\\\#{pdf_file}:PDF"
-  puts "}\r\n\r"
-end
 
 end
 
